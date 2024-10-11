08ace810774162719c1b98bf2b94f623e55f148a
Bijen Thapa <bishal984060@gmail.com>
2024-10-11 22:48:39 +0545

    Dynamic Nav bar implemeted

--- index.js
+++ index.js
@@ -12,6 +12,22 @@ app.get("/", (req, res) => {
   res.render("index");
 });
 
+app.get("/menu", (req, res) => {
+  res.render("menu");
+});
+
+app.get("/about", (req, res) => {
+  res.render("about");
+});
+
+app.get("/courses", (req, res) => {
+  res.render("courses");
+});
+
+app.get("/contact", (req, res) => {
+  res.render("contact");
+});
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
