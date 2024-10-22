e1aa81439a843336f0baee397e4b92e7bec99203
Bijen Thapa <bishal984060@gmail.com>
2024-10-22 14:47:20 +0545

    minor updates

--- src/index.js
+++ src/index.js
@@ -1,23 +1,14 @@
 const express = require("express");
 const session = require("express-session");
 const app = express();
-const port = 3001;
+const port = process.env.port || 3001;
 
 app.set("view engine", "ejs");
-
-app.use(
-  session({
-    secret: "asdf",
-    resave: false,
-    saveUninitialized: false,
-  })
-);
 
 app.use(express.static("views"));
 app.use(express.static("views/style/styles.css"));
 app.use(express.static("views/style/404.css"));
 // app.use(express.static("views/style/login.css"));
-// app.use(express.static("views/style/main.css"));
 
 app.get("/", (req, res) => {
   res.render("index");
